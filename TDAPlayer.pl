551eb5bdcb3064e3b84cb0fda6ad8cfae8cd2362
Matias Ramirez <matias.ramirez.e@usach.cl>
2024-11-14 23:45:37 -0300

    agregando piece y board

--- TDAPlayer.pl
+++ TDAPlayer.pl
@@ -1,5 +1,9 @@
-
+/*
 player(Id, Name, Color, Win, Losses, Draws, Piezas, [Id, Name, Color, Win, Losses, Draws, Piezas]).
 
-
+*/
 getplayer([_, Name, _, _, _, _, _], Name).
+
+
+player(Id, Name, Color, Win, Losses, Draws, Piezas, Player):-
+    Player = [Id, Name, Color, Win, Losses, Draws, Piezas].
