1dfc6767521c89a3d2132ac5bc99364e420bfdf6
Matias Ramirez <matias.ramirez.e@usach.cl>
2024-11-25 22:28:00 -0300

    terminando el proyecto

--- TDAGame.pl
+++ TDAGame.pl
@@ -65,4 +65,88 @@ end_game([Player1,Player2,Board,Turnoactual,Historial],[P1,P2,Board,Turnoactual,
 	write(P1),nl,
 	write(P2),nl.
 %--------------------------------------------------------------
+player_play([Player1,Player2,Board,Turnoactual,Historial],Player,Column,Newgame2):-
+	corresponde_player(Player1,Player),
+	not(no_tienen_fichas(Player1)),
+	get_current_player([Player1,Player2,Board,Turnoactual,Historial],Cp),
+	corresponde_player(Cp,Player),
+	saber_color(Player,Color),
+	piece(Color,Ficha),
+	play_piece(Board,Column,Ficha,Newboard),
+	actualizar_historial(Column,Color,Historial,Newhistorial),
+	Newturno is Turnoactual + 1,
+	restar_fichas(Player1,NP1),
+	game(NP1,Player2,Newboard,Newturno,Newhistorial,Newgame),
+	is_draw(Newgame),
+	end_game(Newgame,Newgame2),
+	imprimir_tablero(Newboard), nl.
+
+player_play([Player1,Player2,Board,Turnoactual,Historial],Player,Column,Newgame2):-
+	corresponde_player(Player2,Player),
+	not(no_tienen_fichas(Player2)),
+	get_current_player([Player1,Player2,Board,Turnoactual,Historial],Cp),
+	corresponde_player(Cp,Player),
+	saber_color(Player,Color),
+	piece(Color,Ficha),
+	play_piece(Board,Column,Ficha,Newboard),
+	actualizar_historial(Column,Color,Historial,Newhistorial),
+	Newturno is Turnoactual + 1,
+	restar_fichas(Player2,NP2),
+	game(Player1,NP2,Newboard,Newturno,Newhistorial,Newgame),
+	is_draw(Newgame),
+	end_game(Newgame,Newgame2),
+	imprimir_tablero(Newboard), nl.
+
+player_play([Player1,Player2,Board,Turnoactual,Historial],Player,Column,Newgame2):-
+	corresponde_player(Player1,Player),
+	not(no_tienen_fichas(Player1)),
+	get_current_player([Player1,Player2,Board,Turnoactual,Historial],Cp),
+	corresponde_player(Cp,Player),
+	saber_color(Player,Color),
+	piece(Color,Ficha),
+	play_piece(Board,Column,Ficha,Newboard),
+	actualizar_historial(Column,Color,Historial,Newhistorial),
+	Newturno is Turnoactual + 1,
+	restar_fichas(Player1,NP1),
+	game(NP1,Player2,Newboard,Newturno,Newhistorial,Newgame),
+	not(is_draw(Newgame)),
+	(algien_gana(Newboard)->
+	(end_game([NP1,Player2,Newboard,Turnoactual,Newhistorial],Newgame2));
+	(game(NP1,Player2,Newboard,Newturno,Newhistorial,Newgame2))),
+	imprimir_tablero(Newboard), nl.
+
+player_play([Player1,Player2,Board,Turnoactual,Historial],Player,Column,Newgame2):-
+	corresponde_player(Player2,Player),
+	not(no_tienen_fichas(Player2)),
+	get_current_player([Player1,Player2,Board,Turnoactual,Historial],Cp),
+	corresponde_player(Cp,Player),
+	saber_color(Player,Color),
+	piece(Color,Ficha),
+	play_piece(Board,Column,Ficha,Newboard),
+	actualizar_historial(Column,Color,Historial,Newhistorial),
+	Newturno is Turnoactual + 1,
+	restar_fichas(Player2,NP2),
+	game(Player1,NP2,Newboard,Newturno,Newhistorial,Newgame),
+	not(is_draw(Newgame)),
+	(algien_gana(Newboard)->
+	(end_game([Player1,NP2,Newboard,Turnoactual,Newhistorial],Newgame2));
+	(game(Player1,NP2,Newboard,Newturno,Newhistorial,Newgame2))),
+	imprimir_tablero(Newboard), nl.
+
+actualizar_historial(Column,Color,Lista,[[Column,Color]|Lista]).
+
+
+saber_color([_,_, Color,_,_,_,_],Color).
+
+restar_fichas([Id1, Name1, Color1, Win1, Losses1, Draws1, Piezas1],
+			[Id1, Name1, Color1, Win1, Losses1, Draws1, PN1]):-
+	PN1 is Piezas1 - 1.
+
+
+corresponde_player([Id,_,_,_,_,_,_],[Id,_,_,_,_,_,_]).
+
+algien_gana(Board):-
+	who_is_winner(Board,Winner),
+	Winner \= 0.
+
 
